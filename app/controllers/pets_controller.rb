37f234c7cd441a473d3ac270edbfc6aec5e19648
okorkov <alex.okorkov@gmail.com>
2020-12-18 13:43:12 -0800

    commit tree be7427be07deafbc1695abf6bf42165f3a395144

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,26 +1,58 @@
 class PetsController < ApplicationController
 
   get '/pets' do
+    
     @pets = Pet.all
     erb :'/pets/index' 
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    if !params["owner"]["name"].empty?
+      @owner = Owner.create(params["owner"])
+    else
+      @owner = Owner.find(params[:owners][:owner][0])
+    end
+    @pet = Pet.new(name: params[:pet_name])
+    @pet.owner_id = @owner
+    @owner.pets << @pet
+    @pet.save
+    @owner.save
     redirect to "pets/#{@pet.id}"
   end
 
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
+    @owner = Owner.find(@pet.owner_id)
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do 
+    @pet = Pet.all.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
 
+
+  patch '/pets/:id' do 
+    puts params
+    @pet = Pet.find(params[:id])
+    if params[:new_owner][:name] == ""
+      @owner = Owner.find(params[:owner][:name])
+    else
+      @owner = Owner.new
+      @owner.name = params[:new_owner][:name]
+    end
+    @pet.name = params[:pet][:name]
+    @owner.pets << @pet
+    @pet.save
+    @owner.save
     redirect to "pets/#{@pet.id}"
   end
+
 end
+
